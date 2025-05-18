5b7cb210260a4077f08afd37660cb4e4dced469e
suisrael <88337104+suisrael@users.noreply.github.com>
2025-05-18 10:25:59 -0400

    version and font changes

--- livewhale-events-block.php
+++ livewhale-events-block.php
@@ -4,7 +4,7 @@
  * Description:       Livewhale Events Block for Gutenberg block generation
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            UFIT Web Services
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
